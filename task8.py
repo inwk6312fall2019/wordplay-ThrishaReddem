0967ad4a885eae42f2892af597180f7929793a28
ThrishaReddem <thrisha.reddem@dal.ca>
2019-09-22 05:22:48 -0700

    task8

---
+++ task8.py
@@ -0,0 +1,15 @@
+def palindrome(number):
+    return str(number) == str(num)[::-1]
+
+
+def eight_times():
+    for num in range(100000, 1000000):
+        if palindrome(str(number)[2:6]):
+            if palindrome(str(number + 1)[1:6]):
+                if palindrome(str(number + 2)[1:5]):
+                    if palindrome(str(number +3)):
+                        print number
+    return False
+
+car eight_times()
+
